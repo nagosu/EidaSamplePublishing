ab79d073366b5564d966d09af6c576ac61134527
nagosu <nozick1021@gmail.com>
2024-05-28 14:17:31 +0900

    친구 추가 모달 퍼블리싱

--- src/pages/AddFriendPage/AddFriendPage.tsx
+++ src/pages/AddFriendPage/AddFriendPage.tsx
@@ -3,40 +3,153 @@
 import { styles } from "./styles";
 import sampleImage from "../../../public/assets/images/sampleImage.jpeg";
 import cancelIcon from "../../../public/assets/images/cancel.png";
+import searchIcon from "../../../public/assets/images/search.png";
+import { useEffect, useState } from "react";
 
 const AddFriendPage = () => {
+  const [activeMode, setActiveMode] = useState<string>("친구");
+  const [isSearching, setIsSearching] = useState<boolean>(false);
+  const [searchData, setSearchData] = useState<string>("");
+
+  // 선택된 버튼
+  const modes = ["친구", "로비", "채널"];
+
+  // 프로필 더미데이터
+  const profiles = [
+    { level: "Lv.10", nickname: "파이썬마스터", image: sampleImage },
+    { level: "Lv.20", nickname: "코딩새싹", image: sampleImage },
+    { level: "Lv.15", nickname: "코딩꿈나무", image: sampleImage },
+    { level: "Lv.25", nickname: "김혜원", image: sampleImage },
+    { level: "Lv.18", nickname: "여은진이", image: sampleImage },
+    { level: "Lv.22", nickname: "오늘도힘차게", image: sampleImage },
+    { level: "Lv.17", nickname: "냐냐냥", image: sampleImage },
+    { level: "Lv.19", nickname: "귀여운게최고", image: sampleImage },
+    { level: "Lv.21", nickname: "꼬잉꼬잉", image: sampleImage },
+    { level: "Lv.23", nickname: "코딩왕", image: sampleImage },
+    { level: "Lv.21", nickname: "파이썬초보", image: sampleImage },
+    { level: "Lv.23", nickname: "코딩왕", image: sampleImage },
+    { level: "Lv.21", nickname: "리액트고수", image: sampleImage },
+    { level: "Lv.23", nickname: "코딩초보", image: sampleImage },
+  ];
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchData(e.target.value);
+  };
+
+  useEffect(() => {
+    if (searchData) {
+      setIsSearching(true);
+    } else {
+      setIsSearching(false);
+    }
+  }, [searchData]);
+
   return (
     <div css={styles.container}>
       <div css={styles.header}>
         <span css={styles.title}>친구 추가</span>
         {/* 닫기 이미지 추가 해야함 */}
       </div>
-      <div css={styles.main}></div>
+      <div css={styles.main}>
+        <div css={styles.mainContent}>
+          <div css={styles.buttonContainer}>
+            {modes.map((mode) => (
+              <button
+                key={mode}
+                css={[
+                  styles.modeButton,
+                  activeMode === mode && styles.activeModeButton,
+                ]}
+                onClick={() => setActiveMode(mode)}
+              >
+                <span css={styles.modeButtonText}>{mode}</span>
+              </button>
+            ))}
+          </div>
+          <div css={styles.nicknameSearchContainer}>
+            <div css={styles.searchContainer}>
+              <input
+                css={styles.searchInput}
+                type='text'
+                placeholder='닉네임 검색'
+                value={searchData}
+                onChange={handleInputChange}
+              />
+              <img css={styles.searchIcon} src={searchIcon} alt='' />
+              {isSearching && (
+                <div css={styles.searchResultContainer}>
+                  <div css={styles.searchResultList}>
+                    <div css={styles.searchResultItemActive}>
+                      <div css={styles.searchProfileItem}>
+                        <div css={styles.profileImageWrapper}>
+                          <img
+                            css={styles.profileImage}
+                            src={sampleImage}
+                            alt=''
+                          />
+                          <div css={styles.profileStatus}></div>
+                        </div>
+                        <span css={styles.profileLevel}>Lv.43</span>
+                        <span>
+                          <span css={styles.highlighText}>코</span>
+                          <span css={styles.profileNickName}>딩고수</span>
+                        </span>
+                      </div>
+                    </div>
+                    {profiles.map((profile, index) => (
+                      <div key={index} css={styles.searchResultItem}>
+                        <div css={styles.searchProfileItem}>
+                          <div css={styles.profileImageWrapper}>
+                            <img
+                              css={styles.profileImage}
+                              src={profile.image}
+                              alt=''
+                            />
+                            <div css={styles.profileStatus}></div>
+                          </div>
+                          <span css={styles.profileLevel}>{profile.level}</span>
+                          <span css={styles.profileNickName}>
+                            {profile.nickname}
+                          </span>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+            <div css={styles.nicknameScroll}>
+              {profiles.map((profile, index) => (
+                <div key={index} css={styles.profileItem}>
+                  <div css={styles.profileImageWrapper}>
+                    <img css={styles.profileImage} src={profile.image} alt='' />
+                    <div css={styles.profileStatus}></div>
+                  </div>
+                  <span css={styles.profileLevel}>{profile.level}</span>
+                  <span css={styles.profileNickName}>{profile.nickname}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
       <div css={styles.footer}>
         <div css={styles.selectedFriendContainer}>
           <span css={styles.selectedFriendTitle}>선택된 친구</span>
           <div css={styles.selectedFriendList}>
-            <div css={styles.selectedFriendItem}>
-              <div css={styles.profileContainer}>
-                <img css={styles.profileImage} src={sampleImage} alt='' />
-                <span css={styles.selectedUserName}>귀여운게최고</span>
+            {profiles.map((profile, index) => (
+              <div key={index} css={styles.selectedFriendItem}>
+                <div css={styles.selectedProfileContainer}>
+                  <img
+                    css={styles.selectedProfileImage}
+                    src={profile.image}
+                    alt=''
+                  />
+                  <span css={styles.selectedUserName}>{profile.nickname}</span>
+                </div>
+                <img css={styles.cancelIcon} src={cancelIcon} alt='' />
               </div>
-              <img css={styles.cancelIcon} src={cancelIcon} alt='' />
-            </div>
-            <div css={styles.selectedFriendItem}>
-              <div css={styles.profileContainer}>
-                <img css={styles.profileImage} src={sampleImage} alt='' />
-                <span css={styles.selectedUserName}>코딩꿈나무</span>
-              </div>
-              <img css={styles.cancelIcon} src={cancelIcon} alt='' />
-            </div>
-            <div css={styles.selectedFriendItem}>
-              <div css={styles.profileContainer}>
-                <img css={styles.profileImage} src={sampleImage} alt='' />
-                <span css={styles.selectedUserName}>오늘도힘차게</span>
-              </div>
-              <img css={styles.cancelIcon} src={cancelIcon} alt='' />
-            </div>
+            ))}
           </div>
         </div>
         <button css={styles.addButton}>
--- src/pages/AddFriendPage/styles.ts
+++ src/pages/AddFriendPage/styles.ts
@@ -12,7 +12,7 @@ const styles = {
   `,
 
   header: css`
-    width: 660px;
+    width: 100%;
     height: 50px;
     border-top-left-radius: 15px;
     border-top-right-radius: 15px;
@@ -35,15 +35,293 @@ const styles = {
   `,
 
   main: css`
-    width: 660px;
+    width: 100%;
     height: 464px;
     padding: 20px 30px 30px;
     display: flex;
     flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `,
+
+  mainContent: css`
+    width: 600px;
+    height: 414px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+
+  buttonContainer: css`
+    width: 100%;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `,
+
+  activeModeButton: css`
+    width: 198px;
+    height: 40px;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    background-color: #3c9aff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+
+  modeButton: css`
+    width: 198px;
+    height: 40px;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    background-color: #d7ddeb;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+
+  modeButtonText: css`
+    font-family: "Pretendard";
+    font-size: 13px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: center;
+    color: #fff;
+  `,
+
+  nicknameSearchContainer: css`
+    width: 600px;
+    height: 375px;
+    padding-top: 21px;
+    padding-left: 15px;
+    padding-right: 15px;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    border: solid 1px #e5e8f0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+
+  searchContainer: css`
+    width: 570px;
+    height: 40px;
+    padding: 8px 15px;
+    border-radius: 20px;
+    background-color: #f4f6fa;
+    display: flex;
+    align-self: start;
+    justify-content: space-between;
+    position: relative;
+  `,
+
+  searchInput: css`
+    width: 500px;
+    height: 24px;
+    font-family: "Pretendard";
+    font-size: 13px;
+    font-weight: 600;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.54;
+    letter-spacing: normal;
+    color: #000;
+    border: none;
+    outline: none;
+    background-color: #f4f6fa;
+
+    &::placeholder {
+      opacity: 0.5;
+      font-family: "Pretendard";
+      font-size: 13px;
+      font-weight: 600;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: 1.54;
+      letter-spacing: normal;
+      color: #000;
+    }
+  `,
+
+  searchIcon: css`
+    width: 24px;
+    height: 24px;
+    object-fit: contain;
+  `,
+
+  searchResultContainer: css`
+    width: 100%;
+    height: 400px;
+    padding-top: 10px;
+    border-radius: 10px;
+    box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.08);
+    background-color: #fff;
+    position: absolute;
+    z-index: 1000;
+    top: 50px;
+    left: 0;
+    overflow-y: hidden;
+  `,
+
+  searchResultList: css`
+    width: 100%;
+    height: 390px;
+    display: flex;
+    flex-direction: column;
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+      width: 3px;
+      right: 10px;
+    }
+
+    &::-webkit-scrollbar-track {
+      border-radius: 2px;
+      right: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      width: 3px;
+      height: 27px;
+      border-radius: 2px;
+      background: #a8b2cc;
+    }
+  `,
+
+  searchResultItem: css`
+    width: 100%;
+    min-height: 40px;
+    padding-left: 15px;
+    padding-right: 15px;
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  `,
+
+  searchResultItemActive: css`
+    width: 100%;
+    min-height: 40px;
+    padding-left: 15px;
+    padding-right: 15px;
+    background-color: #f4f6fa;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  `,
+
+  searchProfileItem: css`
+    width: auto;
+    min-height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  `,
+
+  nicknameScroll: css`
+    width: 570px;
+    height: 315px;
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+      width: 3px;
+      right: 10px;
+    }
+
+    &::-webkit-scrollbar-track {
+      border-radius: 2px;
+      right: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      width: 3px;
+      height: 27px;
+      border-radius: 2px;
+      background: #a8b2cc;
+    }
+  `,
+
+  profileItem: css`
+    width: auto;
+    min-height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    margin-bottom: 10px;
+  `,
+
+  profileImageWrapper: css`
+    width: 24px;
+    height: 24px;
+    margin-right: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+  `,
+
+  profileImage: css`
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    object-fit: contain;
+  `,
+
+  profileStatus: css`
+    width: 8px;
+    height: 8px;
+    border-radius: 4px;
+    border: solid 1px #fff;
+    background-color: #14e14b;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  `,
+
+  profileLevel: css`
+    font-family: "Pretendard";
+    font-size: 14px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.43;
+    letter-spacing: normal;
+    color: #ecb24e;
+    margin-right: 5px;
+  `,
+
+  highlighText: css`
+    font-family: "Pretendard";
+    font-size: 14px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.43;
+    letter-spacing: normal;
+    color: #08f;
+  `,
+
+  profileNickName: css`
+    font-family: "Pretendard";
+    font-size: 14px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.43;
+    letter-spacing: normal;
+    color: #000;
   `,
 
   footer: css`
-    width: 660px;
+    width: 100%;
     height: 86px;
     padding-left: 30px;
     padding-right: 30px;
@@ -78,14 +356,15 @@ const styles = {
   `,
 
   selectedFriendList: css`
-    width: 490px;
+    width: 480px;
     height: 30px;
     display: flex;
     align-items: center;
+    overflow: hidden;
   `,
 
   selectedFriendItem: css`
-    width: 134px;
+    min-width: 134px;
     height: 30px;
     margin-right: 20px;
     display: inline-flex;
@@ -93,12 +372,12 @@ const styles = {
     justify-content: space-between;
   `,
 
-  profileContainer: css`
+  selectedProfileContainer: css`
     display: flex;
     align-items: center;
   `,
 
-  profileImage: css`
+  selectedProfileImage: css`
     width: 30px;
     height: 30px;
     margin-right: 10px;
